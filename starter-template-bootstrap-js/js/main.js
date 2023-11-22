a0c2fce02160d36bbade60e62a038505c67eee57
ValerioTorto <148449060+ValerioTorto@users.noreply.github.com>
2023-11-22 14:20:58 +0100

    Add files via upload

---
+++ starter-template-bootstrap-js/js/main.js
@@ -0,0 +1,9 @@
+// Questo è il file che contiene tutto il codice JS della pagina web
+
+document.writeln("<h3>Ciao Mondo!</h3>");
+
+alert("Hello Word!");
+
+document.getElementById("title_page").innerHTML = "Ciao Mondo!";
+
+console.log("Ciao Mondo!");
